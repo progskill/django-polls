0c978f79f84cdd2049bc6e6bd6046b9052adcfa9
Evans-mutuku <evansnyamai98@gmail.com>
2024-05-08 13:26:46 +0300

    django polls project

---
+++ mysite/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    return HttpResponse("placeholder to display all the surveys created")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+# Create your views here.
